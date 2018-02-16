b98913ca01826ecfab4b1e304c86f86ae03e12f4
vvn050 <nikolov.vladislav@gmail.com>
2018-02-16 10:44:15 +0200

    bug fix for version checker

--- versionChecker.js
+++ versionChecker.js
@@ -2,11 +2,18 @@ let process = require('process');
 let pjson = require('./package.json');
 
 module.exports = function () {
-    let currentVersion = parseFloat(process.versions.node);
-    let requiredVersion = parseFloat(pjson.engines.minimumNodeVersion);
-    let packageName = pjson.name;
+    let currentVersion = process.versions.node,
+        requiredVersion = pjson.engines.minimumNodeVersion,
+        packageName = pjson.name;
 
-    if (currentVersion < requiredVersion) {
+    let currentMajorVersion = Number.parseInt(currentVersion.split('.')[0]),
+        currentMinorVersion = Number.parseInt(currentVersion.split('.')[1]),
+        requiredMajorVersion = Number.parseInt(requiredVersion.split('.')[0]),
+        requiredMinorVersion = Number.parseInt(requiredVersion.split('.')[1]);
+
+    const currentVersionIsCompatible = (currentMajorVersion > requiredMajorVersion) || ((currentMajorVersion === requiredMajorVersion) && (currentMinorVersion >= requiredMinorVersion));
+
+    if (!currentVersionIsCompatible) {
         console.log(`Current version of node js is ${currentVersion}, while the required one is: ${requiredVersion}. ${packageName} is leaving.`);
         process.exit(1);
     }
